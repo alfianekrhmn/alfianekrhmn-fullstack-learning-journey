20d0dca334585a802b2f30fe9c40315977a69c7c
alfianekrhmn <alfian.eka481@gmail.com>
2025-09-17 19:46:09 +0700

    feat: add basic JavaScript variables and operators practice

--- 02-html-semantic/script.js
+++ 02-html-semantic/script.js
@@ -1,2 +1,27 @@
 console.log("hello nama saya Alfian")
 alert("Selamat datang di website saya")
+
+let fullname = "Alfian Eka Rahman"
+let age = 21
+let hobby = "Playing a Game"
+const dream = "Web3 Developer"
+
+console.log("My fullname's " + fullname);
+console.log("i'm " + age + " years old");
+console.log("My hobby is " + hobby);
+console.log("My dream is to be a " + dream);
+
+let firstNumber = 78;
+let secondNumber = 65;
+
+let sum = firstNumber + secondNumber;
+console.log("The result of adding " + firstNumber + " and " + secondNumber + " is " + sum)
+
+if(firstNumber > secondNumber) {
+    console.log("hello world")
+}
+
+let trueOrNot = true
+if(trueOrNot) {
+    console.log("This is true")
+}
