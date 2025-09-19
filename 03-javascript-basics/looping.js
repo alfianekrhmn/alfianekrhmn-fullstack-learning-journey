f81ed107ffaaf524913f6a64738b8c39dbbf7c70
alfianekrhmn <alfian.eka481@gmail.com>
2025-09-19 07:41:43 +0700

    add looping examples with for, while, and for...of

---
+++ 03-javascript-basics/looping.js
@@ -0,0 +1,14 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i)
+// }
+
+let j = 10;
+while(j >= 1){
+    console.log(j)
+    j--
+}
+
+// const hobbies = ['sleep', 'eat', 'code'];
+// for (const hobby of hobbies){
+//     console.log(hobby)
+// }
