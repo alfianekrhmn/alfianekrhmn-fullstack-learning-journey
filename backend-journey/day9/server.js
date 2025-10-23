1bf327ffea7ff18d4a8350b61484d28368cb4030
alfianekrhmn <alfian.eka481@gmail.com>
2025-10-23 22:23:52 +0700

    feat: initialize Express.js server and add basic routes (home, about, contact)

---
+++ backend-journey/day9/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+
+// Home route
+app.get('/', (req, res) => {
+    res.end('Welcome to Home Page!')
+})
+
+// About route
+app.get('/about', (req, res) => {
+    res.end('This is the About Page!')
+})
+
+// Contact route
+app.get('/contact', (req, res) => {
+    res.end('This is the Contact Page!')
+})
+
+// Start server
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
